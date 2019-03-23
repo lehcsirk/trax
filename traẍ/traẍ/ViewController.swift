9a17b45585bc67543b7d6d1b4e8f036060039c9b
Cameron Krischel <ckrischel@he3labs.com>
2019-03-22 18:05:47 -0700

    Add files via upload
    Blank initial upload

---
+++ traẍ/traẍ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  traẍ
+//
+//  Created by Cameron Krischel on 3/22/19.
+//  Copyright © 2019 Cameron Krischel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.text = "I'm a test label"
+        self.view.addSubview(label)
+        
+    }
+
+
+}
+
